577c560cb101cdf382f22bf39b51419354e2d678
malinsjoblom <malin.sjoblom@hotmail.com>
2021-08-20 14:08:49 +0200

    Malin

--- src/components/AppRoute.js
+++ src/components/AppRoute.js
@@ -6,7 +6,7 @@ import Checkout from './Bookings/Checkout';
 import Register from './Login/Register';
 import AddToProduct from './Products/AddToProduct';
 import ForgotPassword from './Login/ForgotPassword';
-import API from './API';
+
 import {Route, BrowserRouter as Router, Switch} from 'react-router-dom'
 import UserProfile from './Login/UserProfile';
 
@@ -17,7 +17,6 @@ export default function AppRoute(){
         <>
 
         <Router>
-
             <Menu />
             <Switch>
             <Route path='/'exact component={ProductList} />
@@ -26,7 +25,6 @@ export default function AppRoute(){
             <Route exact path='/checkout' component={Checkout} />
             <Route path='/register' component={Register} />
             <Route path='/forgotpassword' component= {ForgotPassword} />
-            <Route path='/API' component={API} />
             <Route path='/userprofile' component={UserProfile} />
             </Switch>
         </Router>
