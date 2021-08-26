2f30837dc0f4716a8e34d4d9e3a5024fc918e28b
malinsjoblom <malin.sjoblom@hotmail.com>
2021-08-26 09:36:19 +0200

    config

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import {Link, useHistory } from 'react-router-dom';
 import axios from 'axios';
+import server from "../config"
 
 function Login() {
 
